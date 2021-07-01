4409d193e2e2ea410b79dfd1249e835aaa8b8e99
Gabriel Malta <gabriels.malta@outlook.com>
2021-06-30 22:31:40 -0300

    work queues example

---
+++ src/work-queues/NewTask/NewTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("task_queue", false, false, false, arguments: null);
+
+                string message = GetMessage(args);
+                byte[] body = Encoding.UTF8.GetBytes(message);
+                
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                
+                channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+            }
+        }
+        static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ src/work-queues/Worker/Worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("task_queue", false, false, false, arguments: null);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+                };
+                channel.BasicConsume("task_queue", true, consumer: consumer);
+            }
+            Console.WriteLine("Listening...");
+            Console.ReadLine();
+        }
+    }
+}
